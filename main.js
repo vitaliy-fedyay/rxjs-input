e1bb788a9ea6f6e94fceef2dff72df281d2aeb5c
vit.grey <fedyay.vitaliy@gmail.com>
2019-12-15 21:12:22 +0200

    push error

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-const { fromEvent } = rxjs;
-const { map, debounceTime } = rxjs.operators;
+const { fromEvent } = rxjs
+const { map, debounceTime, distinct, filter } = rxjs.operators
 
-const firstInput = document.getElementById("first-input");
-const secondInput = document.getElementById("second-input");
-const thirdInput = document.getElementById("third-input");
+const firstInput = document.getElementById("first-input")
+const secondInput = document.getElementById("second-input")
+const thirdInput = document.getElementById("third-input")
 
-const firstResult = document.getElementById("first-p");
-const secondResult = document.getElementById("first-p");
-const thirdResult = document.getElementById("first-p");
+const result = document.getElementById("result")
 
-const resfirstInput = fromEvent(firstInput, "input").pipe(
+let data = []
+
+const resultFirstInput = fromEvent(firstInput, "input").pipe(
+  debounceTime(1000),
+  distinct()
+)
+resultFirstInput.subscribe(value => this.data.push(value))
+
+const resultSecondInput = fromEvent(secondInput, "input").pipe(
   debounceTime(1000),
   map(event => event.target.value * 2)
 )
-resfirstInput.subscribe(res => (firstResult.innerHTML = res));
-////////////////////////////////////////////////////////////////////////
-const resfirstInput = fromEvent(firstInput, "input").pipe(
-    debounceTime(1000),
-    map(event => event.target.value * 2)
-  )
-  resfirstInput.subscribe(res => (secondResult.innerHTML = res));
-////////////////////////////////////////////////////////////////////////
-  const resfirstInput = fromEvent(firstInput, "input").pipe(
-    debounceTime(1000),
-    map(event => event.target.value * 2)
-  )
-  resfirstInput.subscribe(res => (thirdResult.innerHTML = res));
+resultSecondInput.subscribe(value => this.data.push(value))
+
+const resultThirdInput = fromEvent(thirdInput, "input").pipe(
+  debounceTime(1000),
+  filter(value => value.trim())
+)
+resultThirdInput.subscribe(value => this.data.push(value))
+
+fromEvent( document, 'keypress').subscribe( event => {
+  if( event.key === 13) {
+    this.result.innerHTML = data
+  }
+})
+
+
